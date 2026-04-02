1f31d29ef1503b930b03ff8f0037d3ffda479bbd
AshmithaPP <ashmitha048@gmail.com>
2026-04-02 19:16:46 +0530

    aboutus page remaining sections added

--- src/features/contact/pages/ContactPage.jsx
+++ src/features/contact/pages/ContactPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ContactHero from '../components/ContactHero/ContactHero';
+import ContactFeatures from '../components/ContactFeatures/ContactFeatures';
+import ContactForm from '../components/ContactForm/ContactForm';
+import NewsletterSection from '@/features/home/components/NewsletterSection/NewsletterSection';
 
 const ContactPage = () => {
     return (
         <div className="contact_page">
             <ContactHero />
-            {/* Other contact sections can be added here */}
+            <ContactFeatures />
+            <ContactForm />
+             <NewsletterSection />
         </div>
     );
 };
